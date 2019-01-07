96fad5db9aac24ab1290f9c818f3e06f58555584
Broyojo <dha@xoba.com>
2019-01-06 21:15:54 -0500

    Initial commit

---
+++ plugins/SerpentGeometrydashGamePlugin/plugin.py
@@ -0,0 +1,28 @@
+import offshoot
+
+
+class SerpentGeometrydashGamePlugin(offshoot.Plugin):
+    name = "SerpentGeometrydashGamePlugin"
+    version = "0.1.0"
+
+    libraries = []
+
+    files = [
+        {"path": "serpent_Geometrydash_game.py", "pluggable": "Game"}
+    ]
+
+    config = {
+        "fps": 30
+    }
+
+    @classmethod
+    def on_install(cls):
+        print("\n\n%s was installed successfully!" % cls.__name__)
+
+    @classmethod
+    def on_uninstall(cls):
+        print("\n\n%s was uninstalled successfully!" % cls.__name__)
+
+
+if __name__ == "__main__":
+    offshoot.executable_hook(SerpentGeometrydashGamePlugin)
